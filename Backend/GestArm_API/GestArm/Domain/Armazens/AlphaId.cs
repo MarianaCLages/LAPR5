f59871b802bb70ce14c25f1e3292db371c8b25fa
Miguel Jordão <1201487@isep.ipp.pt>
2022-10-30 23:50:02 +0000

    L2S35-102 : Alteração do dominio Armazem (Alpha ID)

--- Backend/GestArm_API/GestArm/Domain/Armazens/AlphaId.cs
+++ Backend/GestArm_API/GestArm/Domain/Armazens/AlphaId.cs
@@ -1,23 +1,24 @@
 using GestArm.Domain.Shared;
+using System.Text.RegularExpressions;
 
 namespace GestArm.Domain.Armazens;
 
 public class AlphaId : ValueObject
 {
-
     public AlphaId(string alphaNumId)
     {
+        checkAlphaId(alphaNumId);
         AlphaNumId = alphaNumId;
     }
 
     public string AlphaNumId { get; }
 
-    private void checkAlphaId()
+    private void checkAlphaId(string alphaNumId)
     {
+        Regex rg = new Regex(@"^[a-zA-Z0-9\s,]*$");
 
-        if (AlphaNumId.Length != 3)
+        if (alphaNumId.Length != 3 && !rg.IsMatch(alphaNumId))
             throw new BusinessRuleValidationException("O AlphaId excede o número de caracteres.");
-
     }
 
     protected override IEnumerable<object> GetEqualityComponents()
