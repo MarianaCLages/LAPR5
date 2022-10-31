8d59025127459573628f2e7fcf84dd1b73d4b595
Miguel Jordão <1201487@isep.ipp.pt>
2022-10-31 19:22:35 +0000

    L2S35-26 : DtoParsers (Métodos static para testes)

---
+++ Backend/GestArm_API/GestArm/Domain/Armazens/ArmazemDtoParser.cs
@@ -0,0 +1,12 @@
+﻿namespace GestArm.Domain.Armazens;
+
+public class ArmazemDtoParser
+{
+    public static ArmazemDTO convertToDto(Armazem armazem)
+    {
+        return new ArmazemDTO(armazem.Id,armazem.Latitude.Graus,armazem.Latitude.Minutos,armazem.Latitude.Segundos,
+            armazem.Longitude.Graus,armazem.Longitude.Minutos,armazem.Longitude.Segundos,
+            armazem.Designacao.Designacao,armazem.Endereco.Rua,armazem.Endereco.NumeroPorta,armazem.Endereco.CodigoPostal,
+            armazem.Endereco.Cidade,armazem.Endereco.Pais,armazem.AlphaNumId.AlphaNumId);
+    }
+}
---
+++ Backend/GestArm_API/GestArm/Domain/Encomendas/EncomendaDtoParser.cs
@@ -0,0 +1,11 @@
+﻿namespace GestArm.Domain.Encomendas;
+
+//Método para os testes
+public class EncomendaDtoParser
+{
+    public static EncomendaDto convertToDto(Encomenda encomenda)
+    {
+        return new EncomendaDto(encomenda.Id, encomenda.DataEntrega.ToString(), encomenda.MassaEntrega.Massa, encomenda.TempoCarga.Minutos,
+            encomenda.TempoDescarga.Minutos, encomenda.ArmazemId);
+    }
+}
