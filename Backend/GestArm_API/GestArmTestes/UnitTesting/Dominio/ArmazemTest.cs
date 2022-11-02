c3258d49fa5c92b3f34231c09d63b96fb302f18f
Eduardo Sousa <1200920@isep.ipp.pt>
2022-11-02 12:37:51 +0000

    L2S35-109: update on tests

--- Backend/GestArm_API/GestArmTestes/UnitTesting/Dominio/ArmazemTest.cs
+++ Backend/GestArm_API/GestArmTestes/UnitTesting/Dominio/ArmazemTest.cs
@@ -9,300 +9,222 @@ public class ArmazemTest
     public void CreateValidArmazemTest()
     {
         Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-            new CoordenadasArmazem(10, 20,30),
+            new CoordenadasArmazem(10, 20, 30),
             new CoordenadasArmazem(10, 30, 40),
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-        
+
         Assert.NotNull(arm);
-        
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidArmazemIdTest()
     {
-        try
+        Assert.Throws<NullReferenceException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(null),
-                new CoordenadasArmazem(10, 20,30),
+                new CoordenadasArmazem(10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (NullReferenceException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem ID (Referência nula)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidArmazemIdStringVaziaTest()
     {
-        try
+        Assert.Throws<FormatException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(""),
-                new CoordenadasArmazem(10, 20,30),
+                new CoordenadasArmazem(10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (FormatException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem ID (String Vazia)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidLatitudeGrausTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(-10, 20,30),
+                new CoordenadasArmazem(-10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem latitude(graus negativos)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidLatitudeMinutosTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>(() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(10, -20,30),
+                new CoordenadasArmazem(10, -20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem latitude(minutos negativos)
-        }
-        
+        });
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidLatitudeSegundosTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>(() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(10, 20,-30),
+                new CoordenadasArmazem(10, 20, -30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem latitude(segundos negativos)
-        }
-        
+        });
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidLongitudeGrausTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>(() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(10, 20,30),
+                new CoordenadasArmazem(10, 20, 30),
                 new CoordenadasArmazem(-10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem longitude(graus negativos)
-        }
-        
+        });
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidLongitudeMinutosTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(10, 20,30),
+                new CoordenadasArmazem(10, 20, 30),
                 new CoordenadasArmazem(10, -30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem longitude(minutos negativos)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidLongitudeSegundosTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(10, 20,30),
+                new CoordenadasArmazem(10, 20, 30),
                 new CoordenadasArmazem(10, 30, -40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem longitude(segundos negativos)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidDesignacaoArmazemTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(10, 20,30),
+                new CoordenadasArmazem(10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
-                new DesignacaoArmazem(""),
+                new DesignacaoArmazem(null),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar um armazém sem designação(designação vazia)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidDesignacaoEndereçoRuaTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(-10, 20,30),
+                new CoordenadasArmazem(-10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar uma armazem sem um endereço válido (string de rua inválida)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidDesignacaoEndereçoNumeroPortaTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>(() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(-10, 20,30),
+                new CoordenadasArmazem(-10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", -10, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar uma armazem sem um endereço válido (número de porta invalido)
-        }
-        
+        });
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidDesignacaoEndereçoCodigoPostalTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(-10, 20,30),
+                new CoordenadasArmazem(-10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "", "Pourto", "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar uma armazem sem um endereço válido (string de codigo postal invalido)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidDesignacaoEndereçoCidadeTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>((() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(-10, 20,30),
+                new CoordenadasArmazem(-10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
-                new EnderecoArmazem("Rua das flores", 1, "4000-300", "", "Pourtougal"),
+                new EnderecoArmazem("Rua das flores", 1, "4000-300", null, "Pourtougal"),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar uma armazem sem um endereço válido (string de cidade inválida)
-        }
-        
+        }));
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidDesignacaoEndereçoPaisTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>(() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(-10, 20,30),
+                new CoordenadasArmazem(-10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", ""),
                 new AlphaId("A12"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar uma armazem sem um endereço válido (string de pais inválida)
-        }
-        
+        });
     }
-    
+
     [Fact]
     public void CreateArmazemWithInvalidAlphaIdTest()
     {
-        try
+        Assert.Throws<BusinessRuleValidationException>(() =>
         {
             Armazem arm = new Armazem(new ArmazemId(Guid.NewGuid()),
-                new CoordenadasArmazem(-10, 20,30),
+                new CoordenadasArmazem(-10, 20, 30),
                 new CoordenadasArmazem(10, 30, 40),
                 new DesignacaoArmazem("Designação teste"),
                 new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
                 new AlphaId("AAAAAAAAA"));
-        }
-        catch (BusinessRuleValidationException)
-        {
-            //Vai falhar porque não é possível criar uma armazem sem AlphaIdString (string invalida)
-        }
-        
+        });
     }
-    
-    
 }
