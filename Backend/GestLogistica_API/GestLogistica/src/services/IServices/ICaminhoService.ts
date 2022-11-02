a74496d819f4d4d9a0b689874dff159689c1915a
Miguel Jordão <1201487@isep.ipp.pt>
2022-11-02 15:30:51 +0000

    L2S35-35: Alteração da interface do Caminho (método update e delete)

--- Backend/GestLogistica_API/GestLogistica/src/services/IServices/ICaminhoService.ts
+++ Backend/GestLogistica_API/GestLogistica/src/services/IServices/ICaminhoService.ts
@@ -1,12 +1,12 @@
 import { Result } from "../../core/logic/Result";
 import ICaminhoDTO from "../../dto/caminho/ICaminhoDTO";
 import ICriarCaminhoDTO from "../../dto/caminho/ICriarCaminhoDTO";
-import { CaminhoId } from "../../domain/caminho/caminhoId";
+import ICaminhoIdDto from "../../dto/caminho/ICaminhoIdDto";
 
 export default interface ICaminhoService  {
   createCaminho(caminhoDTO: ICriarCaminhoDTO): Promise<Result<ICaminhoDTO>>;
   updateCaminho(caminhoDTO: ICaminhoDTO): Promise<Result<ICaminhoDTO>>;
   getCaminho (caminhoDTO: ICaminhoDTO): Promise<Result<ICaminhoDTO>>;
-  apagaCaminho(caminhoId : CaminhoId): Promise<Result<ICaminhoDTO>>;
   getAllCaminhos();
+  apagaCaminho(caminhoId : ICaminhoIdDto): Promise<Result<ICaminhoDTO>>;
 }
