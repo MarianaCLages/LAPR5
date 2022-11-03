4497c228b1a8592576afea0f836a193b05efa63d
ines_ribeiro_1201346 <1201346@isep.ipp.pt>
2022-11-03 18:36:16 +0000

    L2S35-32 Update na camada de persistência

--- Backend/GestLogistica_API/GestLogistica/src/dataschema/ICamiaoPersistence.ts
+++ Backend/GestLogistica_API/GestLogistica/src/dataschema/ICamiaoPersistence.ts
@@ -2,9 +2,9 @@ export interface ICamiaoPersistence {
     id : string;
     caractCamiao : string;
     matriculaCamiao : string;
-    capacidadeCarga : string;
-    cargaMaxima : string;
-    cargaTotal : string;
-    tara : string;
-    tempoCarregamento : string;
+    capacidadeCarga : number;
+    cargaMaxima : number;
+    cargaTotal : number;
+    tara : number;
+    tempoCarregamento : number;
 }
