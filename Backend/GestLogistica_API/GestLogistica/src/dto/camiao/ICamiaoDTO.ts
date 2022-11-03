680efe35beac144b0657b770ca4793550427c63e
ines_ribeiro_1201346 <1201346@isep.ipp.pt>
2022-11-03 18:35:51 +0000

    L2S35-32 Update na camada DTO

--- Backend/GestLogistica_API/GestLogistica/src/dto/camiao/ICamiaoDTO.ts
+++ Backend/GestLogistica_API/GestLogistica/src/dto/camiao/ICamiaoDTO.ts
@@ -3,9 +3,9 @@ export interface ICamiaoDTO {
     id: string;
     caractCamiao: string;
     matriculaCamiao : string;
-    capacidadeCarga : string;
-    cargaMax : string;
-    cargaTotal : string;
-    tara : string;
-    tempoCarregamento : string;
+    capacidadeCarga : number;
+    cargaMax : number;
+    cargaTotal : number;
+    tara : number;
+    tempoCarregamento : number;
 }
--- Backend/GestLogistica_API/GestLogistica/src/dto/camiao/ICriarCamiaoDTO.ts
+++ Backend/GestLogistica_API/GestLogistica/src/dto/camiao/ICriarCamiaoDTO.ts
@@ -2,9 +2,9 @@
 export interface ICriarCamiaoDTO {
     caractCamiao: string;
     matriculaCamiao : string;
-    capacidadeCarga : string;
-    cargaMax: string;
-    cargaTotal : string;
-    tara : string;
-    tempoCarregamento : string;
+    capacidadeCarga : number;
+    cargaMax: number;
+    cargaTotal : number;
+    tara : number;
+    tempoCarregamento : number;
 }
