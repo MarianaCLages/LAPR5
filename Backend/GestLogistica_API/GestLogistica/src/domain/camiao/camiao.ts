19cd729d170164b2c472e6b04f5d36fbad20a1ff
Eduardo Sousa <1200920@isep.ipp.pt>
2022-11-05 13:04:58 +0000

    L2S35-32: Update on error messages: Domain

--- Backend/GestLogistica_API/GestLogistica/src/domain/camiao/camiao.ts
+++ Backend/GestLogistica_API/GestLogistica/src/domain/camiao/camiao.ts
@@ -1,15 +1,15 @@
-import { AggregateRoot } from "../../core/domain/AggregateRoot";
-import { UniqueEntityID } from "../../core/domain/UniqueEntityID";
-import { Result } from "../../core/logic/Result";
-import { Tara} from "./tara";
+import {AggregateRoot} from "../../core/domain/AggregateRoot";
+import {UniqueEntityID} from "../../core/domain/UniqueEntityID";
+import {Result} from "../../core/logic/Result";
+import {Tara} from "./tara";
 import {CapacidadeCarga} from "./capacidadeCarga";
 import {CargaMaxima} from "./cargaMaxima";
 import {TempoCarregamento} from "./tempoCarregamento"
-import { Guard } from "../../core/logic/Guard";
-import { ICriarCamiaoDTO } from "../../dto/camiao/ICriarCamiaoDTO";
-import { CargaTotal } from "./cargaTotal";
-import { CaractCamiao } from "./caractCamiao";
-import { MatriculaCamiao } from "./matriculaCamiao";
+import {Guard} from "../../core/logic/Guard";
+import {ICriarCamiaoDTO} from "../../dto/camiao/ICriarCamiaoDTO";
+import {CargaTotal} from "./cargaTotal";
+import {CaractCamiao} from "./caractCamiao";
+import {MatriculaCamiao} from "./matriculaCamiao";
 
 interface CamiaoProps {
     caractCamiao: CaractCamiao;
@@ -23,98 +23,98 @@ interface CamiaoProps {
 
 export class Camiao extends AggregateRoot<CamiaoProps> {
 
-    get id (): UniqueEntityID {
+    private constructor(props: CamiaoProps, id?: UniqueEntityID) {
+        super(props, id);
+    }
+
+    get id(): UniqueEntityID {
         return this._id;
     }
 
-    get caractCamiao (): CaractCamiao {
+    get caractCamiao(): CaractCamiao {
         return this.props.caractCamiao;
     }
 
-    get matriculaCamiao(): MatriculaCamiao{
+    get matriculaCamiao(): MatriculaCamiao {
         return this.props.matriculaCamiao;
     }
 
-    set matriculaCamiao(value: MatriculaCamiao){
+    set matriculaCamiao(value: MatriculaCamiao) {
         this.props.matriculaCamiao = value;
     }
 
-    get tara (): Tara {
+    get tara(): Tara {
         return this.props.tara;
     }
 
-    set tara(value: Tara){
+    set tara(value: Tara) {
         this.props.tara = value;
     }
 
-    get capacidadeCarga (): CapacidadeCarga {
+    get capacidadeCarga(): CapacidadeCarga {
         return this.props.capacidadeCarga;
     }
 
-    set capacidadeCarga (value: CapacidadeCarga){
+    set capacidadeCarga(value: CapacidadeCarga) {
         this.props.capacidadeCarga = value;
     }
 
-    get cargaMax (): CargaMaxima {
+    get cargaMax(): CargaMaxima {
         return this.props.cargaMax;
     }
 
-    set cargaMax (value: CargaMaxima){
+    set cargaMax(value: CargaMaxima) {
         this.props.cargaMax = value;
     }
 
-    get cargaTotal (): CargaTotal {
+    get cargaTotal(): CargaTotal {
         return this.props.cargaTotal;
     }
 
-    set cargaTotal (value: CargaTotal){
+    set cargaTotal(value: CargaTotal) {
         this.props.cargaTotal = value;
     }
 
-    get tempoCarregamento (): TempoCarregamento {
+    get tempoCarregamento(): TempoCarregamento {
         return this.props.tempoCarregamento;
     }
 
-    set tempoCarregamento (value: TempoCarregamento){
+    set tempoCarregamento(value: TempoCarregamento) {
         this.props.tempoCarregamento = value;
     }
 
-    private constructor (props: CamiaoProps, id?: UniqueEntityID) {
-        super (props, id);
-    }
-
-    public static create (camiaoDTO: ICriarCamiaoDTO, id?:UniqueEntityID): Result <Camiao> {
+    public static create(camiaoDTO: ICriarCamiaoDTO, id?: UniqueEntityID): Result<Camiao> {
 
         const guardedProps = [
-            {argument: camiaoDTO.caractCamiao, argumentName: 'caractCamiao' },
-            {argument: camiaoDTO.matriculaCamiao, argumentName: 'matriculaCamiao' },
+            {argument: camiaoDTO.caractCamiao, argumentName: 'caractCamiao'},
+            {argument: camiaoDTO.matriculaCamiao, argumentName: 'matriculaCamiao'},
             {argument: camiaoDTO.tara, argumentName: "tara"},
             {argument: camiaoDTO.capacidadeCarga, argumentName: "capacidadeCarga"},
             {argument: camiaoDTO.cargaMax, argumentName: "cargaMax"},
             {argument: camiaoDTO.cargaTotal, argumentName: "cargaTotal"},
             {argument: camiaoDTO.tempoCarregamento, argumentName: "tempoCarregamento"}
-            
+
         ];
 
         const guardResult = Guard.againstNullOrUndefinedBulk(guardedProps);
 
         if (!guardResult.succeeded || !RegExp('^[A-Z]{2}-[0-9]{2}-[A-Z]{2}$').test(camiaoDTO.matriculaCamiao)) {
-            return Result.fail<Camiao>(guardResult.message)
-        }     
-        else {
+            return Result.fail<Camiao>("Camiao inválido");
+        } else {
             const camiao = new Camiao({
-                caractCamiao: new CaractCamiao({value: camiaoDTO.caractCamiao}),
-                matriculaCamiao: new MatriculaCamiao({value: camiaoDTO.matriculaCamiao}),
-                tara: new Tara ({value: camiaoDTO.tara}),
-                capacidadeCarga: new CapacidadeCarga ({value: camiaoDTO.capacidadeCarga}),
-                cargaMax: new CargaMaxima ({value: camiaoDTO.cargaMax}),
-                cargaTotal: new CargaTotal ({value: camiaoDTO.cargaTotal}),
-                tempoCarregamento: new TempoCarregamento ({value: camiaoDTO.tempoCarregamento})},
+                    caractCamiao: new CaractCamiao({value: camiaoDTO.caractCamiao}),
+                    matriculaCamiao: new MatriculaCamiao({value: camiaoDTO.matriculaCamiao}),
+                    tara: new Tara({value: camiaoDTO.tara}),
+                    capacidadeCarga: new CapacidadeCarga({value: camiaoDTO.capacidadeCarga}),
+                    cargaMax: new CargaMaxima({value: camiaoDTO.cargaMax}),
+                    cargaTotal: new CargaTotal({value: camiaoDTO.cargaTotal}),
+                    tempoCarregamento: new TempoCarregamento({value: camiaoDTO.tempoCarregamento})
+                },
                 id
             );
 
-        return Result.ok<Camiao>(camiao);
+            return Result.ok<Camiao>(camiao);
+        }
     }
-}
 
 }
--- Backend/GestLogistica_API/GestLogistica/src/dto/caminho/ICaminhoArmazemChegadaIdDTO.ts
+++ Backend/GestLogistica_API/GestLogistica/src/dto/caminho/ICaminhoArmazemChegadaIdDTO.ts
@@ -1,3 +1,3 @@
 export default interface ICaminhoArmazemChegadaIdDTO {
-    caminhoArmazemChegadaId: string
+    armazemChegadaId: string
 }
