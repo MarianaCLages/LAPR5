24476fb003fac3f98f1d9749e3c630a288068ce1
ines_ribeiro_1201346 <1201346@isep.ipp.pt>
2022-11-05 00:21:22 +0000

    L2S35-36 Update Listar Caminhos - criação de DTOs para guardar o ID de armazens

---
+++ Backend/GestLogistica_API/GestLogistica/src/dto/caminho/ICaminhoArmazemChegadaIdDTO.ts
@@ -0,0 +1,3 @@
+export default interface ICaminhoArmazemChegadaIdDTO {
+    caminhoArmazemChegadaId: string
+}
---
+++ Backend/GestLogistica_API/GestLogistica/src/dto/caminho/ICaminhoArmazemPartidaIdDTO.ts
@@ -0,0 +1,3 @@
+export default interface ICaminhoArmazemPartidaIdDTO {
+    caminhoArmazemPartidaId: string
+}
