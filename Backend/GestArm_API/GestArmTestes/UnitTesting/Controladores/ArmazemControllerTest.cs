b32f7ed348f1935ae97425f18c2711e4d97c06a0
Miguel Jordão <1201487@isep.ipp.pt>
2022-11-05 20:48:58 +0000

    L2S35-109 : #comment Update nos testes do controlador (Unit testing)

--- Backend/GestArm_API/GestArmTestes/UnitTesting/Controladores/ArmazemControllerTest.cs
+++ Backend/GestArm_API/GestArmTestes/UnitTesting/Controladores/ArmazemControllerTest.cs
@@ -1,6 +1,5 @@
 using DDDNetCore.Controllers;
 using GestArm.Domain.Armazens;
-using GestArm.Domain.Shared;
 using Moq;
 using Newtonsoft.Json;
 
@@ -10,18 +9,20 @@ public class ArmazemControllerTest
 {
     private readonly ArmazemController _controller;
     private readonly Mock<IArmazemService> _ServiceMock = new();
-
-    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
-
-
+    
     public ArmazemControllerTest()
     {
         _controller = new ArmazemController(_ServiceMock.Object);
     }
 
+    /**
+     * Gets all armazens available, using the GetAllAsync(mocking the service) returning a ArmazemDTO
+     */
+    
     [Fact]
-    public void GetAllAsyncTest()
+    public void GetAllAsyncTest_ShouldGetAllArmazens()
     {
+        //ARRANGE
         var listDto = new List<ArmazemDTO>();
 
         var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
@@ -30,27 +31,32 @@ public class ArmazemControllerTest
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-
-
+        
         var armDto = ArmazemDtoParser.convertToDto(arm);
-
         listDto.Add(armDto);
-
+        
+        //ACT
         _ServiceMock.Setup(x => x.GetAllAsync()).ReturnsAsync(listDto);
         var result = _controller.GetAll().Result;
 
         var objExpected = result.Value.First();
         var objActual = listDto.First();
-
+        
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-
+        
+        //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
+    
+    /**
+     * Gets all armazens by designação, using the GetByDesignacaoAsync(mocking the service) returning a ArmazemDTO
+     */
 
     [Fact]
-    public void GetByDesignacaoTest()
+    public void GetByDesignacaoTest_ShouldGetAllArmazensByDesignacao()
     {
+        //ARRANGE
         var listDto = new List<ArmazemDTO>();
 
         var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
@@ -62,9 +68,9 @@ public class ArmazemControllerTest
 
 
         var armDto = ArmazemDtoParser.convertToDto(arm);
-
         listDto.Add(armDto);
-
+        
+        //ACT
         _ServiceMock.Setup(x => x.GetByDesignacaoAsync(arm.Designacao.Designacao)).ReturnsAsync(listDto);
         var result = _controller.GetByDesignacao(arm.Designacao.Designacao).Result;
 
@@ -73,13 +79,20 @@ public class ArmazemControllerTest
 
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-
+        
+        //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
 
+    /**
+     * Gets an armazem by ID, using the GetByIdAsync(mocking the service) returning a ArmazemDTO
+     */
+    
     [Fact]
-    public void GetByIdTest()
+    public void GetByIdTest_ShouldReturnAnArmazemByID()
     {
+        
+        //ARRANGE
         var listDto = new List<ArmazemDTO>();
 
         var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
@@ -91,9 +104,9 @@ public class ArmazemControllerTest
 
 
         var armDto = ArmazemDtoParser.convertToDto(arm);
-
         listDto.Add(armDto);
 
+        //ACT
         _ServiceMock.Setup(x => x.GetByIdAsync(arm.Id)).ReturnsAsync(armDto);
         var result = _controller.GetById(arm.Id.AsGuid()).Result;
 
@@ -103,13 +116,54 @@ public class ArmazemControllerTest
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
 
+        //ARRANGE
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
+    
+    /**
+     * Gets an armazem by Domain ID, using the GetByArmazemIdAsync(mocking the service) returning a ArmazemDTO
+     */
+    
+    [Fact]
+    public void GetByArmazemDomainId_ShouldReturnAnArmazemByDomainID()
+    {
+        
+        //ARRANGE
+        var listDto = new List<ArmazemDTO>();
+
+        var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
+            new CoordenadasArmazem(10, 20, 30),
+            new CoordenadasArmazem(10, 30, 40),
+            new DesignacaoArmazem("Designação teste"),
+            new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
+            new AlphaId("A12"));
+
+
+        var armDto = ArmazemDtoParser.convertToDto(arm);
+        listDto.Add(armDto);
+
+        //ACT
+        _ServiceMock.Setup(x => x.GetByArmazemIdAsync(arm.AlphaNumId.AlphaNumId)).ReturnsAsync(armDto);
+        var result = _controller.GetByArmazemIdAsync(arm.AlphaNumId.AlphaNumId).Result;
+
+        var objExpected = result.Value;
+        var objActual = listDto.First();
+
+        var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
+        var obj2StrActual = JsonConvert.SerializeObject(objActual);
+
+        //ARRANGE
+        Assert.Equal(obj1StrExpected, obj2StrActual);
+    }
+    
+    /**
+     * Adds an armazem , using the AddAsync(mocking the service) returning a ArmazemDTO
+     */
 
     [Fact]
-    public void AddAsyncTest()
+    public void AddAsyncTest_ShouldAddAnArmazem()
     {
-
+        //ARRANGE
         var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
         new CoordenadasArmazem(12, 13, 14),
         new CoordenadasArmazem(15, 12, 13),
@@ -121,8 +175,8 @@ public class ArmazemControllerTest
 
         CreatingArmazemDto createDTO = new CreatingArmazemDto(12, 13, 14, 15, 12, 13, "Designação teste",
         "Rua das flores", 1, "4000-300", "Pourto", "Pourtougal", "A12");
-
-
+        
+        //ACT
         _ServiceMock.Setup(x => x.AddAsync(createDTO)).ReturnsAsync(armDto);
         var result = _controller.AddAsync(createDTO).Result;
 
@@ -131,16 +185,70 @@ public class ArmazemControllerTest
 
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-
-        Console.WriteLine(obj1StrExpected);
-        Console.WriteLine(obj2StrActual);
-
+        
+        //ACT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
+    
+    /**
+     * Deletes an armazem , using the DeleteAsync(mocking the service) returning false (fail test case)
+     */
 
     [Fact]
-    public void DeleteAsyncTest()
+    public void DeleteAsyncTest_ShouldReturnFalse()
     {
-        //TODO: implementar este teste
+        //ARRANGE
+        var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
+            new CoordenadasArmazem(12, 13, 14),
+            new CoordenadasArmazem(15, 12, 13),
+            new DesignacaoArmazem("Designação teste"),
+            new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
+            new AlphaId("A12"));
+        
+        //ACT
+        _ServiceMock.Setup(x => x.DeleteAsync(arm.Id)).ReturnsAsync(false);
+        var result = _controller.DeleteAsync(arm.Id.AsGuid()).Result;
+
+        var objExpected = false;
+        var objActual = result.Value;
+        
+        //ACT
+        Assert.Equal(objExpected, objActual);
     }
+    
+    /**
+     * Updates an armazem specifing certain parameters, using the UpdateAsync(mocking the service), returning an ArmazemDTO
+     */
+    
+    [Fact]
+    public void UpdateAsyncTest_ShouldUpdateAnArmazem()
+    {
+        //ARRANGE
+        var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
+            new CoordenadasArmazem(12, 13, 14),
+            new CoordenadasArmazem(15, 12, 13),
+            new DesignacaoArmazem("Designação teste"),
+            new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
+            new AlphaId("A12"));
+
+        var armDto = ArmazemDtoParser.convertToDto(arm);
+
+        CreatingArmazemDto createDTO = new CreatingArmazemDto(12, 13, 14, 15, 12, 13, "Designação teste",
+            "Rua das flores", 1, "4000-300", "Pourto", "Pourtougal", "A12");
+        
+        //ACT
+        _ServiceMock.Setup(x => x.UpdateAsync(armDto)).ReturnsAsync(armDto);
+        _ServiceMock.Setup(x => x.AddAsync(createDTO)).ReturnsAsync(armDto);
+        var result = _controller.AddAsync(createDTO).Result;
+
+        var objExpected = armDto;
+        var objActual = result.Value;
+
+        var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
+        var obj2StrActual = JsonConvert.SerializeObject(objActual);
+        
+        //ACT
+        Assert.Equal(obj1StrExpected, obj2StrActual);
+    }
+    
 }
--- Backend/GestArm_API/GestArmTestes/UnitTesting/Controladores/EncomendaControllerTest.cs
+++ Backend/GestArm_API/GestArmTestes/UnitTesting/Controladores/EncomendaControllerTest.cs
@@ -1,6 +1,5 @@
 using GestArm.Controllers;
 using GestArm.Domain.Encomendas;
-using GestArm.Domain.Shared;
 using Moq;
 using Newtonsoft.Json;
 
@@ -10,27 +9,57 @@ public class EncomendaControllerTest
 {
     private readonly EncomendaController _controller;
     private readonly Mock<IEncomendasService> _ServiceMock = new();
-
-    private readonly Mock<IUnitOfWork> _unitOfWorkMock = new();
-
-
+    
     public EncomendaControllerTest()
     {
         _controller = new EncomendaController(_ServiceMock.Object);
     }
-
+    
+    /**
+     * Gets an encomenda by it's specific ID, using the GetByIdAsync(mocking the service) returning a EncomendaDTO
+     */
+    
     [Fact]
-    public void GetAllAsyncTest()
+    public void GetByIdEncomenda_ShouldReturnAnEncomenda()
     {
+        //ARRANGE
+        var en = new Encomenda(new EncomendaDomainId("2", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
+            new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
+
+        var encomendaDto = EncomendaDtoParser.convertToDto(en);
+        
+        //ACT
+        _ServiceMock.Setup(x => x.GetByIdAsync(en.Id)).ReturnsAsync(encomendaDto);
+
+        var result = _controller.GetById(en.Id.AsGuid()).Result;
+
+        var objExpected = result.Value;
+        var objActual = encomendaDto;
+
+        var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
+        var obj2StrActual = JsonConvert.SerializeObject(objActual);
+        
+        //ASSERT
+        Assert.Equal(obj1StrExpected, obj2StrActual);
+    }
+
+    /**
+     * Gets all the encomendas available, using the GetAllAsync(since we are mocking the service we will only return a list with one Encomenda) returning a List of EncomendaDTO
+     */
+    
+    [Fact]
+    public void GetAllAsyncTest_ShouldReturnAllEncomendas()
+    {
+        //ARRANGE
         var listDto = new List<EncomendaDto>();
 
         var en = new Encomenda(new EncomendaDomainId("2", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
             new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
 
         var encomendaDto = EncomendaDtoParser.convertToDto(en);
-
         listDto.Add(encomendaDto);
 
+        //ACT
         _ServiceMock.Setup(x => x.GetAllAsync()).ReturnsAsync(listDto);
 
         var result = _controller.GetAllAsync().Result;
@@ -40,22 +69,28 @@ public class EncomendaControllerTest
 
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-
+        
+        //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
+    
+    /**
+     * Gets an encomenda by it's specific Armazem ID, using the GetByArmazemIdAsync(mocking the service) returning a EncomendaDTO
+     */
 
     [Fact]
-    public void GetByArmazemIdAysncTest()
+    public void GetByArmazemIdAysncTest_ShouldReturnAnEntregaWhichArmazemIDIsUnique()
     {
+        //ARRANGE
         var listDto = new List<EncomendaDto>();
 
         var en = new Encomenda(new EncomendaDomainId("3", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
             new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
 
         var encomendaDto = EncomendaDtoParser.convertToDto(en);
-
         listDto.Add(encomendaDto);
 
+        //ACT
         _ServiceMock.Setup(x => x.GetByArmazemIdAsync(en.ArmazemId)).ReturnsAsync(listDto);
         var result = _controller.GetByArmazemIdAysnc(en.ArmazemId).Result;
 
@@ -65,21 +100,27 @@ public class EncomendaControllerTest
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
 
+        //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
+    
+    /**
+     * Gets an encomenda by it's specific Delivery Date, using the GetByDataEntregaAysnc(mocking the service) returning a EncomendaDTO
+     */
 
     [Fact]
-    public void GetByDataDeEntregaAysncTest()
+    public void GetByDataDeEntregaAysncTest_ShouldGetAllEntregasWithCertainData()
     {
+        //ARRANGE
         var listDto = new List<EncomendaDto>();
 
         var en = new Encomenda(new EncomendaDomainId("4", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
             new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
 
         var encomendaDto = EncomendaDtoParser.convertToDto(en);
-
         listDto.Add(encomendaDto);
 
+        //ACT
         _ServiceMock.Setup(x => x.GetByDataEntregaAysnc(en.DataEntrega.Data)).ReturnsAsync(listDto);
         var result = _controller.GetByDataDeEntregaAysnc(en.DataEntrega.Data).Result;
 
@@ -89,21 +130,27 @@ public class EncomendaControllerTest
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
 
+        //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
-
+    
+    /**
+     * Gets an encomenda by a specific filter, using the GetByFiltragemAysnc(mocking the service) returning a EncomendaDTO
+     */
+    
     [Fact]
-    public void GetByFiltragemAysncTest()
+    public void GetByFiltragemAysncTest_ShouldReturnAllWithCertainFiltragem()
     {
+        //ARRANGE
         var listDto = new List<EncomendaDto>();
 
         var en = new Encomenda(new EncomendaDomainId("5", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
             new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
 
         var encomendaDto = EncomendaDtoParser.convertToDto(en);
-
         listDto.Add(encomendaDto);
 
+        //ACT
         _ServiceMock.Setup(x => x.GetByFiltragemAysnc(en.ArmazemId, en.DataEntrega.Data)).ReturnsAsync(listDto);
         var result = _controller.GetByFiltragemAysnc(en.ArmazemId, en.DataEntrega.Data.ToString()).Result;
 
@@ -113,18 +160,93 @@ public class EncomendaControllerTest
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
 
+        //ASSERT
+        Assert.Equal(obj1StrExpected, obj2StrActual);
+    }
+    
+    /**
+     * Adds an encomenda with certain values, using the AddAsyncTest(mocking the service) returning a EncomendaDTO
+     */
+
+    [Fact]
+    public void AddAsyncTest_ShouldAddAnArmazem()
+    {
+        //ARRANGE
+
+        var en = new Encomenda(new EncomendaDomainId("5", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
+            new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
+
+
+        CreatingEncomendaDto creatingEncomendaDto = new CreatingEncomendaDto("2022-12-27", 10, 120, 120, "A12");
+        
+        var encomendaDto = EncomendaDtoParser.convertToDto(en);
+
+        //ACT
+        _ServiceMock.Setup(x => x.AddAsync(creatingEncomendaDto)).ReturnsAsync(encomendaDto);
+        var result = _controller.AddAsync(creatingEncomendaDto).Result;
+
+        var objExpected = encomendaDto;
+        var objActual = encomendaDto;
+
+        var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
+        var obj2StrActual = JsonConvert.SerializeObject(objActual);
+
+        //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
 
+    /**
+     * Removes an encomenda specifing a certain ID, using the RemoveAsync(mocking the service), returning "TRUE"
+     */
+    
     [Fact]
-    public void AddAsyncTest()
+    public void DeleteAsyncTest_ShouldDeleteAnArmazemReturningTrue()
     {
-        //TODO: implementar este teste
-    }
+        //ARRANGE
+        var en = new Encomenda(new EncomendaDomainId("5", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
+            new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
+        
+        //ACT
+        _ServiceMock.Setup(x => x.RemoveAsync(en.Id)).ReturnsAsync(true);
+        var result = _controller.DeleteAsync(en.Id.AsGuid()).Result;
 
-    [Fact]
-    public void DeleteAsyncTest()
-    {
-        //TODO: implementar este teste
+        var objExpected = true;
+        var objActual = result.Value;
+
+        var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
+        var obj2StrActual = JsonConvert.SerializeObject(objActual);
+
+        //ASSERT
+        Assert.Equal(obj1StrExpected, obj2StrActual);
     }
+    
+    /**
+     * Updates an encomenda specifing certain parameters, using the UpdateAsync(mocking the service), returning an EncomendaDTO
+     */
+    
+    [Fact]
+    public void UpdateAsyncTest_ShouldUpdateAnArmazemReturningAnEncomendaDTO()
+    {
+        //ARRANGE
+        var en = new Encomenda(new EncomendaDomainId("5", "221227"), new DataEntrega(DateTime.Parse("2022-12-27")),
+            new MassaEntrega(10), new TempoEncomenda(120), new TempoEncomenda(120), "A12");
+        
+        var creatingEncomendaDto = new CreatingEncomendaDto("2022-12-27", 10, 120, 120, "A12");
+
+        var encomendaDto = EncomendaDtoParser.convertToDto(en);
+        
+        //ACT
+        _ServiceMock.Setup(x => x.UpdateAsync(en.Id,creatingEncomendaDto)).ReturnsAsync(encomendaDto);
+        var result = _controller.Update(en.Id.AsGuid(),creatingEncomendaDto).Result;
+
+        var objExpected = encomendaDto;
+        var objActual = encomendaDto;
+
+        var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
+        var obj2StrActual = JsonConvert.SerializeObject(objActual);
+
+        //ASSERT
+        Assert.Equal(obj1StrExpected, obj2StrActual);
+    }
+    
 }
