28767753b00a67edb2412abe467bc5b5200aea51
ines_ribeiro_1201346 <1201346@isep.ipp.pt>
2022-11-05 19:10:01 +0000

    L2S35-32 Update Criar Camião - testes unitários ao domínio

---
+++ Backend/GestLogistica_API/GestLogistica/tests/unit/domain/camiao/camiao.test.ts
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import { Camiao } from "../../../../src/domain/camiao/camiao";
+
+describe('Camiao', () => {
+    it('should create a new Camiao', () => {
+        const camiaoDTO = {
+            domainId: 'domainId',
+            caractCamiao: 'caractCamiao',
+            matriculaCamiao: 'AA-56-BB',
+            tara: 1,
+            capacidadeCarga: 1,
+            cargaMax: 1,
+            cargaTotal: 1,
+            tempoCarregamento: 1
+        };
+
+        const camiao = Camiao.create(camiaoDTO);
+
+        expect(camiao.isSuccess).to.be.true;
+    });
+
+    it('should not create a new Camiao with invalid caractCamiao', () => {
+        const camiaoDTO = {
+            domainId: 'domainId',
+            caractCamiao: '',
+            matriculaCamiao: 'matriculaCamiao',
+            tara: 1,
+            capacidadeCarga: 1,
+            cargaMax: 1,
+            cargaTotal: 1,
+            tempoCarregamento: 1
+        };
+
+        const camiao = Camiao.create(camiaoDTO);
+
+        expect(camiao.isSuccess).to.be.false;
+    });
+
+    it('should not create a new Camiao with invalid matriculaCamiao', () => {
+        const camiaoDTO = {
+            domainId: 'domainId',
+            caractCamiao: 'caractCamiao',
+            matriculaCamiao: '19-19-19',
+            tara: 1,
+            capacidadeCarga: 1,
+            cargaMax: 1,
+            cargaTotal: 1,
+            tempoCarregamento: 1
+        };
+
+        const camiao = Camiao.create(camiaoDTO);
+
+        expect(camiao.isSuccess).to.be.false;
+    });
+});
