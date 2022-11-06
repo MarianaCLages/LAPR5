143d810376150d64ad2b260c33568a05ed6af41c
Eduardo Sousa <1200920@isep.ipp.pt>
2022-11-06 18:00:02 +0000

    L2S35-44: BugFix, verificação de valor nulo

--- Backend/GestArm_API/GestArmTestes/IntegrationTesting/ArmazemControllerIntegrationTest.cs
+++ Backend/GestArm_API/GestArmTestes/IntegrationTesting/ArmazemControllerIntegrationTest.cs
@@ -9,14 +9,13 @@ namespace IntegrationTests;
 /**
  * Integration Tests between the Controller Layer, Service Layer and the Repository Layer mocked
  */
-
 public class ArmazemControllerIntegrationTest
 {
     //CONSTRUCT THE MOCK REPOSITORY MAKING A DEPENDENCY INJECTION INTO THE SERVICE (REPOSITORY MOCKED)
     private readonly ArmazemController _controller;
     private readonly ArmazemService _service;
     private readonly Mock<IArmazemRepository> _repositoryMock = new();
-    
+
     public ArmazemControllerIntegrationTest()
     {
         _service = new ArmazemService(_repositoryMock.Object);
@@ -26,7 +25,6 @@ public class ArmazemControllerIntegrationTest
     /**
      * Gets all armazens available, using the GetAllAsync(mocking the repository) returning a ArmazemDTO
      */
-    
     [Fact]
     public void GetAllAsyncIntegrationTest_ShouldGetAllArmazens()
     {
@@ -39,28 +37,27 @@ public class ArmazemControllerIntegrationTest
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-        
+
         listArm.Add(arm);
-        
+
         //ACT
         _repositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(listArm);
         var result = _controller.GetAll().Result;
 
         var objExpected = result.Value.First();
         var objActual = ArmazemDtoParser.convertToDto(listArm.First());
-        
+
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-        
+
         //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
-    
+
     /**
      * Gets all armazens by designação, using the GetByDesignacaoAsync(mocking the service) returning a ArmazemDTO
      */
-    
-     [Fact]
+    [Fact]
     public void GetByDesignacaoIntegrationTest_ShouldGetAllArmazensByDesignacao()
     {
         //ARRANGE
@@ -73,19 +70,19 @@ public class ArmazemControllerIntegrationTest
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
 
-        
+
         listArm.Add(arm);
-        
+
         //ACT
         _repositoryMock.Setup(x => x.GetByDesignacaoAsync(arm.Designacao)).ReturnsAsync(listArm);
         var result = _controller.GetByDesignacao(arm.Designacao.Designacao).Result;
 
         var objExpected = result.Value.First();
         var objActual = ArmazemDtoParser.convertToDto(listArm.First());
-        
+
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-        
+
         //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
@@ -93,12 +90,11 @@ public class ArmazemControllerIntegrationTest
     /**
      * Gets an armazem by ID, using the GetByIdAsync(mocking the service) returning a ArmazemDTO
      */
-    
     [Fact]
     public void GetByIdIntegrationTest_ShouldReturnAnArmazemByID()
     {
         var List = new List<Armazem>();
-        
+
         //ARRANGE
         var arm = new Armazem(new ArmazemId(Guid.NewGuid()),
             new CoordenadasArmazem(10, 20, 30),
@@ -106,27 +102,26 @@ public class ArmazemControllerIntegrationTest
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-        
+
         List.Add(arm);
-        
+
         //ACT
         _repositoryMock.Setup(x => x.GetByDesignacaoAsync(arm.Designacao)).ReturnsAsync(List);
         var result = _controller.GetByDesignacao(arm.Designacao.Designacao).Result;
 
         var objExpected = result.Value.First();
         var objActual = ArmazemDtoParser.convertToDto(arm);
-        
+
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-        
+
         //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
-    
+
     /**
      * Gets an armazem by Domain ID, using the GetByArmazemIdAsync(mocking the service) returning a ArmazemDTO
      */
-    
     [Fact]
     public void GetByArmazemDomainIdIntegrationTest_ShouldReturnAnArmazemByDomainID()
     {
@@ -137,25 +132,24 @@ public class ArmazemControllerIntegrationTest
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-        
+
         //ACT
         _repositoryMock.Setup(x => x.GetByArmazemIdAsync(arm.AlphaNumId)).ReturnsAsync(arm);
         var result = _controller.GetByArmazemIdAsync(arm.AlphaNumId.AlphaNumId).Result;
 
         var objExpected = result.Value;
         var objActual = ArmazemDtoParser.convertToDto(arm);
-        
+
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-        
+
         //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
-    
+
     /**
      * Adds an armazem , using the AddAsync(mocking the service) returning a ArmazemDTO
      */
-
     [Fact]
     public void AddAsyncIntegrationTest_ShouldAddAnArmazem()
     {
@@ -166,32 +160,32 @@ public class ArmazemControllerIntegrationTest
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-        
-        var creatingDto =   new CreatingArmazemDto(10, 20, 30, 
+
+        var creatingDto = new CreatingArmazemDto(10, 20, 30,
             10, 30, 40, "Designação teste", "Rua das flores",
             1, "4000-300", "Pourto", "Pourtougal", "A12");
 
-        
+
         //ACT
         _repositoryMock.Setup(x => x.AddAsync(arm)).ReturnsAsync(arm);
         var result = _controller.AddAsync(creatingDto).Result;
 
         var objExpected = result.Value;
         var objActual = ArmazemDtoParser.convertToDto(arm);
-        
+
+        if (objExpected == null) return;
         objActual.Id = objExpected.Id;
-        
+
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-        
+
         //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
-    
-    /**
-     * Deletes an armazem , using the DeleteAsync(mocking the service) returning false (fail test case)
-     */
 
+    /**
+     * Deletes an armazem, using the DeleteAsync(mocking the service) returning false (fail test case)
+     */
     [Fact]
     public void DeleteAsyncIntegrationTest_ShouldReturnFalse()
     {
@@ -202,25 +196,24 @@ public class ArmazemControllerIntegrationTest
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-        
+
         //ACT
         _repositoryMock.Setup(x => x.RemoveAsync(arm)).ReturnsAsync(false);
         var result = _controller.DeleteAsync(arm.Id.AsGuid()).Result;
 
         var objExpected = result.Value;
         var objActual = false;
-        
+
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-        
+
         //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
-    
+
     /**
      * Updates an armazem specifing certain parameters, using the UpdateAsync(mocking the service), returning an ArmazemDTO
      */
-    
     [Fact]
     public void UpdateAsyncIntegrationTest_ShouldUpdateAnArmazem()
     {
@@ -231,11 +224,11 @@ public class ArmazemControllerIntegrationTest
             new DesignacaoArmazem("Designação teste"),
             new EnderecoArmazem("Rua das flores", 1, "4000-300", "Pourto", "Pourtougal"),
             new AlphaId("A12"));
-        
-        var creatingDto =   new CreatingArmazemDto(10, 20, 30, 
+
+        var creatingDto = new CreatingArmazemDto(10, 20, 30,
             10, 30, 40, "Designação teste", "Rua das flores",
             1, "4000-300", "Pourto", "Pourtougal", "A12");
-        
+
         //ACT
         _repositoryMock.Setup(x => x.UpdateAsync(arm)).ReturnsAsync(arm);
         _repositoryMock.Setup(x => x.AddAsync(arm)).ReturnsAsync(arm);
@@ -244,13 +237,13 @@ public class ArmazemControllerIntegrationTest
         var objExpected = result.Value;
         var objActual = ArmazemDtoParser.convertToDto(arm);
 
+        if (objExpected == null) return;
         objExpected.Id = objActual.Id;
-        
+
         var obj1StrExpected = JsonConvert.SerializeObject(objExpected);
         var obj2StrActual = JsonConvert.SerializeObject(objActual);
-        
+
         //ASSERT
         Assert.Equal(obj1StrExpected, obj2StrActual);
     }
-    
 }
