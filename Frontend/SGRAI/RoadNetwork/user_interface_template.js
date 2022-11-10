5e94fb6be74c8ede0f14a4933d9701ff7545170f
Mariana Lages <1200902@isep.ipp.pt>
2022-11-10 14:33:25 +0000

    L2S35-122: Implementação inicial da user interface (a ser alterado posteriormente)

---
+++ Frontend/SGRAI/RoadNetwork/user_interface_template.js
@@ -0,0 +1,84 @@
+import * as THREE from "three";
+import { GUI } from "../three.js-master/examples/jsm/libs/lil-gui.module.min.js";
+
+export default class UserInteraction {
+    constructor(scene, renderer, lights, fog, object, animations) {
+
+        function colorCallback(object, color) {
+            object.color.set(color);
+        }
+
+        function shadowsCallback(enabled) {
+            scene.traverseVisible(function (child) { // Modifying the scene graph inside the callback is discouraged: https://threejs.org/docs/index.html?q=object3d#api/en/core/Object3D.traverseVisible
+                if (child.material) {
+                    if (child.material instanceof THREE.Material) {
+                        child.material.needsUpdate = true;
+                    }
+                    else if (child.material instanceof Array) {
+                        child.material.forEach(element => {
+                            if (element instanceof THREE.Material) {
+                                element.needsUpdate = true;
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
+        function createEmoteCallback(animations, name) {
+            callbacks[name] = function () {
+                animations.fadeToAction(name, 0.2);
+            };
+            emotesFolder.add(callbacks, name);
+        }
+
+        // Create the graphical user interface
+        this.gui = new GUI({ hideable: false });
+
+        // Create the lights folder
+        const lightsFolder = this.gui.addFolder("Lights");
+
+        // Create the ambient light folder
+        const ambientLightFolder = lightsFolder.addFolder("Ambient light");
+        const ambientLight = lights.object.ambientLight;
+        const ambientColor = { color: "#" + new THREE.Color(ambientLight.color).getHexString() };
+        ambientLightFolder.addColor(ambientColor, "color").onChange(color => colorCallback(ambientLight, color));
+        ambientLightFolder.add(lights.object.ambientLight, "intensity", 0.0, 1.0, 0.01);
+
+        // Create point light #1 folder
+        const pointLight1Folder = lightsFolder.addFolder("Point light #1");
+        const pointLight1 = lights.object.pointLight1;
+        const pointColor1 = { color: "#" + new THREE.Color(pointLight1.color).getHexString() };
+        pointLight1Folder.addColor(pointColor1, "color").onChange(color => colorCallback(pointLight1, color));
+        pointLight1Folder.add(lights.object.pointLight1, "intensity", 0.0, 1.0, 0.01);
+        pointLight1Folder.add(lights.object.pointLight1, "distance", 0.0, 20.0, 0.01);
+        pointLight1Folder.add(lights.object.pointLight1.position, "x", -10.0, 10.0, 0.01);
+        pointLight1Folder.add(lights.object.pointLight1.position, "y", 0.0, 20.0, 0.01);
+        pointLight1Folder.add(lights.object.pointLight1.position, "z", -10.0, 10.0, 0.01);
+
+        // Create point light #2 folder
+        const pointLight2Folder = lightsFolder.addFolder("Point light #2");
+        const pointLight2 = lights.object.pointLight2;
+        const pointColor2 = { color: "#" + new THREE.Color(pointLight2.color).getHexString() };
+        pointLight2Folder.addColor(pointColor2, "color").onChange(color => colorCallback(pointLight2, color));
+        pointLight2Folder.add(lights.object.pointLight2, "intensity", 0.0, 1.0, 0.01);
+        pointLight2Folder.add(lights.object.pointLight2, "distance", 0.0, 20.0, 0.01);
+        pointLight2Folder.add(lights.object.pointLight2.position, "x", -10.0, 10.0, 0.01);
+        pointLight2Folder.add(lights.object.pointLight2.position, "y", 0.0, 20.0, 0.01);
+        pointLight2Folder.add(lights.object.pointLight2.position, "z", -10.0, 10.0, 0.01);
+
+        // Create the shadows folder
+        const shadowsFolder = this.gui.addFolder("Shadows");
+        shadowsFolder.add(renderer.shadowMap, "enabled").onChange(enabled => shadowsCallback(enabled));
+
+    }
+
+    setVisibility(visible) {
+        if (visible) {
+            this.gui.show();
+        }
+        else {
+            this.gui.hide();
+        }
+    }
+}
