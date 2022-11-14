1ae5b44fcdc01bff2b852e67bc37e35d84141872
Mariana Lages <1200902@isep.ipp.pt>
2022-11-14 12:47:36 +0000

    L2S35-128 : ALGAV US2 done (needs revision)

--- Backend/Prolog_API/predicatesB.pl
+++ Backend/Prolog_API/predicatesB.pl
@@ -1,13 +1,13 @@
 :-include('warehousesBD.pl').
 :-include('truckBD.pl').
 
-% Find all paths in our BD
+% Find all paths in our DB.
 % getAllPaths/3 (<L - List with all the warehouses that we want to
 % permutate>, <LLP - Result of the findAll>, <R - Final Result with the
 % Matosinhos warehouse at the beggining and at the end>).
 
-getAllPaths(L,LLP,R):-findall(LP,permutation(L,LP),LLP),
-                      fix(LLP,R).
+getAllPaths(L,LLP,R) :- findall(LP,permutation(L,LP),LLP),
+                        addMatosinhos(LLP,R).
 
 % Adds the Matosinhos warehouse to all paths, assuring that they
 % all start in the warehouse number "5" (Matosinhos) and end in it
@@ -17,9 +17,26 @@ getAllPaths(L,LLP,R):-findall(LP,permutation(L,LP),LLP),
 % warehouse>).
 
 addMatosinhos([],[]).
-addMatosinhos([H|T],[H1|T1]):-append(H,[5],HR),
-                              append([5],HR,H1),
-                              addMatosinhos(T,T1).
+addMatosinhos([H|T],[H1|T1]) :- append(H,[5],HR),
+                                append([5],HR,H1),
+                                addMatosinhos(T,T1).
 
 
+% Sums the total time for each path.
+% sumTime/2 (<[O,D|C] - path list>, <R - path's time>).
 
+sumTime([_],0) :- !.
+sumTime([O,D|C],R) :- dadosCam_t_e_ta(_,O,D,T,_,_),
+                      sumTime([D|C],R1),
+                      R is R1 + T.
+
+% Gets the smallest time.
+% smallestTime/3 (<[L|C] - paths list>, R - <most efficient path>, <M -
+% time of the most efficient path>).
+
+smallestTime([L],L,T) :- sumTime(L,T),!.
+smallestTime([H|T],R,M) :- sumTime(H,F),
+                           smallestTime(T,R1,M1),
+                           ((F < M1,!,R = H, M is F);
+                           (F > M1,!,R = R1, M is M1);
+                           (R = [H,R1], M is F)).
