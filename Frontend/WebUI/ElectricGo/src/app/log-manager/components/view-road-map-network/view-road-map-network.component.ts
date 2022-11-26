21a6eca63b85346676d1bd5586bdb68ed80c7920
Miguel Jordão <1201487@isep.ipp.pt>
2022-11-26 22:50:11 +0000

    L2S35-122: #time 1d #comment Refactoring the code

--- Frontend/WebUI/ElectricGo/src/app/log-manager/components/view-road-map-network/view-road-map-network.component.ts
+++ Frontend/WebUI/ElectricGo/src/app/log-manager/components/view-road-map-network/view-road-map-network.component.ts
@@ -7,10 +7,13 @@ import {
   ViewChild,
 } from '@angular/core';
 
+//import Stats from 'stats.js';
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as THREE from 'three';
 
 import jsonInfo from './roadMap/roadMap.json';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-view-road-map-network',
@@ -32,17 +35,14 @@ export class ViewRoadMapNetworkComponent implements OnInit {
 
   //ANIMATION
 
-  @Input() public rotationSpeedX: number = 0.05;
-
-  @Input() public rotationSpeedY: number = 0.01;
-
   @Input() public size: number = 200;
 
-  @Input() public texture: string = './assets/texture.jpg';
+  //Road Texture
+  @Input() public texture: string = './roadMap/road_free.png';
 
   //STAGE PROPERTIES
 
-  @Input() public cameraZ: number = 8000;
+  @Input() public cameraZ: number = 6000;
 
   @Input() public fieldOfView: number = 1;
 
@@ -57,23 +57,8 @@ export class ViewRoadMapNetworkComponent implements OnInit {
   }
 
   private loader = new THREE.TextureLoader();
-  private geometry = new THREE.BoxGeometry(1, 1, 1);
-  private material = new THREE.MeshBasicMaterial({ color: '#00FF00' });
 
-  private cube: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
-
-  private cirGeometry: THREE.CircleGeometry = new THREE.CircleGeometry(
-    (2.1 * 1) / 2,
-    32
-  );
-  private cirMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
-    side: THREE.DoubleSide,
-    color: '#00FF00',
-  });
-  private roundabout: THREE.Mesh = new THREE.Mesh(
-    this.cirGeometry,
-    this.cirMaterial
-  );
+  private roundabout!: THREE.Mesh;
 
   private render!: THREE.WebGLRenderer;
 
@@ -85,93 +70,179 @@ export class ViewRoadMapNetworkComponent implements OnInit {
 
   private createRoadMap() {
     this.roadMap = new THREE.Group();
-
     this.createRoundAbout();
+    this.scene.add(this.roadMap);
+    //this.roadMap.scale.set(2,2,2);
 
+    //DEFINE THE BACK GROUND COLOR
+    this.scene.background = new THREE.Color(0xadd8e6);
+
+    // ROUNDABOUTS
     for (let i = 0; i < jsonInfo.map.length; i++) {
-      const geometry = new THREE.CircleGeometry((2.1 * 1) / 2, 32); // alturar o 1 para wi (largura do nó)
-      const material = new THREE.MeshBasicMaterial({
-        side: THREE.DoubleSide,
-      });
+      let roundabouT = this.roundabout.clone();
 
-      let roundabouT = new THREE.Mesh(geometry, material);
-
-      roundabouT.rotation.x = -Math.PI / 2.0;
-      roundabouT.scale.set(1.5, 1.5, 1.5);
-
-      roundabouT.castShadow = true;
-      roundabouT.receiveShadow = true;
-
+      //Set the position of the roundabout
       roundabouT.position.set(
         jsonInfo.map[i][0],
         jsonInfo.map[i][1],
         jsonInfo.map[i][2]
       );
 
-      this.scene.add(roundabouT);
+      this.roadMap.add(roundabouT);
     }
 
-    // ROADS
-    for (let i = 0; i < jsonInfo.roads.length; i++) {
-      for (let j = 0; j < jsonInfo.roads[i].length; j++) {
-        if (jsonInfo.roads[i][j] != 0) {
-          let roadLength = Math.sqrt(
-            Math.pow(jsonInfo.map[j][0] - jsonInfo.map[i][0], 2) +
-              Math.pow(jsonInfo.map[j][1] - jsonInfo.map[i][1], 2) +
-              Math.pow(jsonInfo.map[j][2] - jsonInfo.map[i][2], 2)
-          );
+    const circleConstant = 2;
+    const connectionConstant = 0.5;
 
-          const circleConstant = 2;
-          const connectionConstant = 0.5;
+    const connectionLength = connectionConstant * circleConstant;
 
-          let connectionLength = connectionConstant * circleConstant;
+    const radius = 2.1;
 
-          let angle =
-            Math.sqrt(
-              Math.pow(jsonInfo.map[j][0] - jsonInfo.map[i][0], 2) +
-                Math.pow(jsonInfo.map[j][1] - jsonInfo.map[i][1], 2)
-            ) -
-            connectionLength * 2;
+    // const roadTexture = new THREE.TextureLoader().load(
+    //   'Frontend/WebUI/ElectricGo/src/app/log-manager/components/view-road-map-network/roadMap/road_1.jpg'
+    // );
 
-          let roadGeometry = new THREE.PlaneGeometry(
-            0.2,
-            roadLength,
-            32
-          );
-          let roadMaterial = new THREE.MeshBasicMaterial({
-            color: 0x00ff00,
-            side: THREE.DoubleSide,
-          });
-          let road = new THREE.Mesh(roadGeometry, roadMaterial);
-          road.position.set(
-            (jsonInfo.map[j][0] + jsonInfo.map[i][0]) / 2,
-            (jsonInfo.map[j][1] + jsonInfo.map[i][1]) / 2,
-            (jsonInfo.map[j][2] + jsonInfo.map[i][2]) / 2
-          );
+    for (let i = 0; i < jsonInfo.paths.length; i++) {
+      //INFO
+      let initWarehouse = jsonInfo.paths[i][0] - 1;
+      let finalWarehouse = jsonInfo.paths[i][1] - 1;
+      let roadWidth = jsonInfo.paths[i][2];
 
-          road.rotation.z =
-            Math.atan2(jsonInfo.map[j][1] - jsonInfo.map[i][1], jsonInfo.map[j][0] - jsonInfo.map[i][0]) -
-            Math.PI / 2;
+      // //Incoming Edges
+      const material2 = new THREE.LineBasicMaterial({ color: 0xff0000 });
 
-          road.rotateOnAxis(
-            new THREE.Vector3(1, 0, 0),
-            Math.atan2(jsonInfo.map[j][2] - jsonInfo.map[i][2], angle)
-          );
+      let points = [];
+      let points2 = [];
 
-          this.scene.add(road);
-        }
-      }
+      const difX =
+        jsonInfo.map[initWarehouse][0] - jsonInfo.map[finalWarehouse][0];
+      const difY =
+        jsonInfo.map[initWarehouse][1] - jsonInfo.map[finalWarehouse][1];
+      const difZ =
+        jsonInfo.map[initWarehouse][2] - jsonInfo.map[finalWarehouse][2];
+
+      points.push(
+        new THREE.Vector3(
+          jsonInfo.map[initWarehouse][0],
+          jsonInfo.map[initWarehouse][1],
+          jsonInfo.map[initWarehouse][2]
+        )
+      );
+      points.push(
+        new THREE.Vector3(
+          jsonInfo.map[initWarehouse][0] - 0.1 * difX,
+          jsonInfo.map[initWarehouse][1] - 0.1 * difY,
+          jsonInfo.map[initWarehouse][2]
+        )
+      );
+      points2.push(
+        new THREE.Vector3(
+          jsonInfo.map[finalWarehouse][0],
+          jsonInfo.map[finalWarehouse][1],
+          jsonInfo.map[finalWarehouse][2]
+        )
+      );
+      points2.push(
+        new THREE.Vector3(
+          jsonInfo.map[finalWarehouse][0] + 0.1 * difX,
+          jsonInfo.map[finalWarehouse][1] + 0.1 * difY,
+          jsonInfo.map[finalWarehouse][2]
+        )
+      );
+
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+      const geometry2 = new THREE.BufferGeometry().setFromPoints(points2);
+
+      const line = new THREE.Line(geometry, material2);
+      const line2 = new THREE.Line(geometry2, material2);
+
+      this.roadMap.add(line);
+      this.roadMap.add(line2);
+
+      //Outgoing Edges
+
+      //Incoming Edge final
+      let finalWarehousePosX = jsonInfo.map[finalWarehouse][0] + 0.1 * difX;
+      let finalWarehousePosY = jsonInfo.map[finalWarehouse][1] + 0.1 * difY;
+      let finalWarehousePosZ = jsonInfo.map[finalWarehouse][2];
+
+      //Incoming Edge init
+      let initWarehousePosX = jsonInfo.map[initWarehouse][0] - 0.1 * difX;
+      let initWarehousePosY = jsonInfo.map[initWarehouse][0] - 0.1 * difY;
+      let initWarehousePosZ = jsonInfo.map[initWarehouse][2];
+
+
+      let roadLength = Math.sqrt(
+        Math.pow(
+          jsonInfo.map[finalWarehouse][0] - jsonInfo.map[initWarehouse][0],
+          2
+        ) +
+          Math.pow(
+            jsonInfo.map[finalWarehouse][1] - jsonInfo.map[initWarehouse][1],
+            2
+          ) +
+          Math.pow(
+            jsonInfo.map[finalWarehouse][2] - jsonInfo.map[initWarehouse][2],
+            2
+          )
+      );
+
+      let angle =
+        Math.sqrt(
+          Math.pow(
+            jsonInfo.map[finalWarehouse][0] - jsonInfo.map[initWarehouse][0],
+            2
+          ) +
+            Math.pow(
+              jsonInfo.map[finalWarehouse][1] - jsonInfo.map[initWarehouse][1],
+              2
+            )
+        ) -
+        connectionLength * 2;
+
+      //Road information
+      let roadGeometry = new THREE.PlaneGeometry(roadWidth, roadLength, 32);
+      let roadMaterial = new THREE.MeshBasicMaterial({
+        color: 0x000000,
+        side: THREE.DoubleSide,
+      });
+
+      let road = new THREE.Mesh(roadGeometry, roadMaterial);
+
+      road.position.set(
+        (jsonInfo.map[finalWarehouse][0] + jsonInfo.map[initWarehouse][0]) / 2,
+        (jsonInfo.map[finalWarehouse][1] + jsonInfo.map[initWarehouse][1]) / 2,
+        (jsonInfo.map[finalWarehouse][2] + jsonInfo.map[initWarehouse][2]) / 2
+      );
+
+      road.rotation.z =
+        Math.atan2(
+          jsonInfo.map[finalWarehouse][1] - jsonInfo.map[initWarehouse][1],
+          jsonInfo.map[finalWarehouse][0] - jsonInfo.map[initWarehouse][0]
+        ) -
+        Math.PI / 2;
+
+      road.rotateOnAxis(
+        new THREE.Vector3(1, 0, 0),
+        Math.atan2(
+          jsonInfo.map[finalWarehouse][2] - jsonInfo.map[initWarehouse][2],
+          angle
+        )
+      );
+
+      this.roadMap.add(road);
     }
   }
 
   private createRoundAbout() {
-    const geometry = new THREE.CircleGeometry((2.1 * 1) / 2, 32); // alturar o 1 para wi (largura do nó)
+    const geometry = new THREE.CircleGeometry((2.1 * 1) / 2, 32);
     const material = new THREE.MeshBasicMaterial({
       side: THREE.DoubleSide,
     });
     this.roundabout = new THREE.Mesh(geometry, material);
 
-    this.roundabout.rotation.x = -Math.PI / 2.0;
+    //this.roundabout.rotation.x = -Math.PI / 2.0;
+
     this.roundabout.scale.set(1.5, 1.5, 1.5);
 
     this.roundabout.castShadow = true;
@@ -187,6 +258,9 @@ export class ViewRoadMapNetworkComponent implements OnInit {
     //this.scene.add(this.cube);
 
     this.createRoadMap();
+
+    //Rotate the scene to a correct angle
+    this.scene.rotation.x = -Math.PI / 2.0;
 
     //CAMERA
     let aspectRatio = this.getAspectRatio();
@@ -209,9 +283,18 @@ export class ViewRoadMapNetworkComponent implements OnInit {
     return this.canvas.clientWidth / this.canvas.clientHeight;
   }
 
-  private animateCube() {
-    this.cube.rotation.x += this.rotationSpeedX;
-    this.cube.rotation.y += this.rotationSpeedY;
+  private calculateCoordinates() {
+    // x = R * cos(lat) * cos(lon)
+
+    // y = R * cos(lat) * sin(lon)
+
+    // z = R *sin(lat)
+
+    const r = 6371;
+
+    // let x = r * Math.cos(lat) * Math.cos(lon);
+    // let y = r * Math.sin(lat) * Math.cos(lon);
+    // let z = r * Math.sin(lat);
   }
 
   private startRenderingLoop() {
@@ -221,16 +304,42 @@ export class ViewRoadMapNetworkComponent implements OnInit {
     this.render.setPixelRatio(devicePixelRatio);
     this.render.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
 
-    // controls
+    // Camera controls
     this.controls = new OrbitControls(this.camera, this.render.domElement);
-    this.controls.enableZoom = false;
-    this.controls.enablePan = false;
+    this.controls.enableZoom = true;
+    this.controls.enablePan = true;
+    this.controls.enableDamping = true;
+
+    let div = document.createElement('div');
+    div.innerHTML = '<br><br><br><br><br><br><br>';
+    document.body.appendChild(div);
+
+    // const stats = new Stats()
+    // stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+    // document.body.appendChild(stats.dom)
+
+    // function animate() {
+
+    //   stats.begin();
+
+    //   // monitored code goes here
+
+    //   stats.end();
+
+    //   requestAnimationFrame( animate );
+
+    // }
 
     let component: ViewRoadMapNetworkComponent = this;
     (function render() {
       requestAnimationFrame(render);
-      component.animateCube();
+      component.controls.update();
       component.render.render(component.scene, component.camera);
+      component.render.setPixelRatio(devicePixelRatio);
+      component.render.setSize(
+        component.canvas.clientWidth,
+        component.canvas.clientHeight
+      );
     })();
   }
 }
