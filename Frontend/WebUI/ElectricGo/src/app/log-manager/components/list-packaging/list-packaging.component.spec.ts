a34bfcdfe135e6fea821352c9acd7d755936f5ca
ines_ribeiro_1201346 <1201346@isep.ipp.pt>
2022-12-01 19:33:59 +0000

    L2S35-119: #comment tests on List Packaging use case

--- Frontend/WebUI/ElectricGo/src/app/log-manager/components/list-packaging/list-packaging.component.spec.ts
+++ Frontend/WebUI/ElectricGo/src/app/log-manager/components/list-packaging/list-packaging.component.spec.ts
@@ -2,24 +2,71 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListPackagingComponent } from './list-packaging.component';
 import {HttpClient, HttpHandler} from "@angular/common/http";
+import { IPackagingDTO } from 'src/app/shared/packagingDTO';
+import { ListPackagingService } from '../../services/list-packaging.service';
 
 describe('ListPackagingComponent', () => {
   let component: ListPackagingComponent;
   let fixture: ComponentFixture<ListPackagingComponent>;
 
   beforeEach(async () => {
+
+    const listPackagingServiceFake = jasmine.createSpyObj('ListPackagingService', ['getPackaging']);
+    listPackagingServiceFake.getPackaging.and.returnValue(Promise.resolve(
+      [
+        {
+          "orderRef": "A1",
+          "truckRef": "A2",
+          "posX": 5,
+          "posY": 1,
+          "posZ": 1,
+        },
+      ],
+    ));
     await TestBed.configureTestingModule({
       declarations: [ ListPackagingComponent ],
-      providers: [HttpClient,HttpHandler]
+      providers: [
+        HttpClient,
+        HttpHandler,
+        { provide: 'GetPackagingService', useValue: listPackagingServiceFake }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ListPackagingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should exist an array of packaging after init', () => {
+    let serviceFake = {
+      getPackaging: () => {
+        //create an IPathDTO array
+        let packaging: IPackagingDTO[] = [
+          {
+            "orderRef": "A1",
+            "truckRef": "A2",
+            "pos3DX": 5,
+            "pos3DY": 1,
+            "pos3DZ": 1,
+          },
+        ];
+        return Promise.resolve(packaging);
+      }
+
+    } as ListPackagingService;
+    component = new ListPackagingComponent(serviceFake);
+    component.ngOnInit();
+
+    fixture.whenStable().then(() => {
+      expect(component.packagings).toBeTruthy();
+
+    });
+  });
+
+  it('should work the back button', () => {
+    component.goBack();
     expect(component).toBeTruthy();
   });
+
 });
