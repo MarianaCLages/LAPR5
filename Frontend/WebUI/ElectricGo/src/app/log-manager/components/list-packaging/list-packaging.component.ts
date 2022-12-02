3eb0157fff951ed0f511fef592378735e766f685
ines_ribeiro_1201346 <1201346@isep.ipp.pt>
2022-12-02 22:42:37 +0000

    L2S35-119: #comment updates on frontEnd for list packagings

--- Frontend/WebUI/ElectricGo/src/app/log-manager/components/list-packaging/list-packaging.component.ts
+++ Frontend/WebUI/ElectricGo/src/app/log-manager/components/list-packaging/list-packaging.component.ts
@@ -14,6 +14,18 @@ import {MatSort} from "@angular/material/sort";
 
 export class ListPackagingComponent implements OnInit {
 
+  
+  orderRef: any;
+  truckRef: any;
+  filterOption: any;
+  dualFilter: any;
+  errorMessage: any;
+  error: boolean = false;
+  options: string[] = [
+    'Packaging Truck',  
+    'Packaging Order',
+    'All Packagings',
+  ];
   packagings = new MatTableDataSource<IPackagingDTO>();
   
   displayedColumns: string[] = ['OrderRef', 'TruckRef', 'xPos', 'yPos', 'zPos'];
@@ -29,20 +41,84 @@ export class ListPackagingComponent implements OnInit {
 
   ngAfterViewInit() {
     // @ts-ignore
-    this.packagings.paginator = this.paginator;
-
-    
+    this.packagings.paginator = this.paginator;    
   }
 
   async ngOnInit(): Promise<void> {
     //calls the service to get the packagigns
     this.listPackagingService.getPackaging().then((data: IPackagingDTO[]) => {
-      console.log(data);
       this.packagings.data = data;
     });
   }
   goBack() {
     window.history.back();
   }
+
+  chooseFilter() {
+    //CLEAR THE FORM
+    this.orderRef = null;
+    this.truckRef = null;
+
+    if (this.filterOption == 'AllPackagings') {
+      this.getPackagingsByFilter();
+  }
 }
+
+  getPackagingsByFilter() {
+    this.errorMessage = '';
+    this.error = false;
+    if (this.filterOption == 'Packaging Order') {
+      this.listPackagingService.getPackagingByOrder(this.orderRef).then((data: any) => {
+        this.packagings = data;
+      },
+      (error: any) => {
+        this.error = true;
+        if (error.status == 404) {
+        this.errorMessage = error.error;
+    } else {
+      if (error.status == 500) {
+        this.errorMessage = error.error.errors.message;
+      } else {
+        this.errorMessage = "Unknown error";
+      }
+    }
+  }
+)
+} else if (this.filterOption == 'Packaging Truck') {
+      this.listPackagingService.getPackagingByTruck(this.truckRef).then((data: any) => {
+        this.packagings = data;
+      },
+      (error: any) => {
+        this.error = true;
+        if (error.status == 404) {
+          this.errorMessage = error.error;
+          } else {
+            if (error.status == 500) {
+              this.errorMessage = error.error.errors.message;
+              } else {
+                this.errorMessage = "Unknown error";
+              }
+            }
+          }
+          );
+        }else if (this.filterOption == 'AllPackagings') {
+          this.listPackagingService.getPackaging().then((data: IPackagingDTO[]) => {
+            this.packagings.data = data;
+            },
+            (error: any) => {
+              this.error = true;
+              if (error.status == 400) {
+                this.errorMessage = error.error;
+                } else {
+                  if (error.status == 500) {
+                    this.errorMessage = error.error.errors.message;
+                    } else {
+                      this.errorMessage = "Unknown error";
+                    }
+                  }
+                }
+        );
+      }
+    }
+  }
 
