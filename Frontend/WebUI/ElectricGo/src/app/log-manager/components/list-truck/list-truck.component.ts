f4259daeea55c0942a87a5a6d850dda2d874713d
Miguel Jordão <1201487@isep.ipp.pt>
2022-12-02 14:53:30 +0000

    L2S35-118: #time 3h #comment Added the filters to the UI (Filter by all/truck-plate/caract-plate)

--- Frontend/WebUI/ElectricGo/src/app/log-manager/components/list-truck/list-truck.component.ts
+++ Frontend/WebUI/ElectricGo/src/app/log-manager/components/list-truck/list-truck.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {GetTrucksService} from '../../../services/get-trucks.service'
-import {ThemePalette} from '@angular/material/core';
-import {MatPaginator} from "@angular/material/paginator";
-import {MatTableDataSource} from "@angular/material/table";
-import {MatSort} from "@angular/material/sort";
-import {ITruckDTO} from '../../../shared/truckDTO';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GetTrucksService } from '../../../services/get-trucks.service';
+import { ThemePalette } from '@angular/material/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { ITruckDTO } from '../../../shared/truckDTO';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 export interface Task {
   name: string;
@@ -19,40 +19,39 @@ export interface Task {
   templateUrl: './list-truck.component.html',
   styleUrls: ['./list-truck.component.css'],
   providers: [GetTrucksService],
-
 })
-
 export class ListTruckComponent {
-
-  subtasks: any[] = [];
-
-  task: Task = {
-    name: 'Indeterminate',
-    completed: false,
-    color: 'primary',
-    subtasks: [
-      {name: 'Filter by Truck Characteristic', completed: false, color: 'primary'},
-      {name: 'Filter by Truck Plate', completed: false, color: 'primary'},
-    ],
-  };
+  truckPlateRef: any;
+  truckCaractRef: any;
+  filterOption: any;
+  dualFilter: any;
+  errorMessage: any;
+  error: boolean = false;
+  options: string[] = [
+    'Truck plate',
+    'Truck characteristic',
+    'All Trucks',
+  ];
 
   trucks = new MatTableDataSource<ITruckDTO>();
-  displayedColumns: string[] = ['Truck characteristic', 'Truck Plate', 'Weight Capacity', 'Max Weight Capacity', 'Max Battery', 'Tare', 'Charging Time'];
+  displayedColumns: string[] = [
+    'Truck characteristic',
+    'Truck Plate',
+    'Weight Capacity',
+    'Max Weight Capacity',
+    'Max Battery',
+    'Tare',
+    'Charging Time',
+  ];
+
   // @ts-ignore
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator ;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   // @ts-ignore
-  @ViewChild(MatSort, {static: true}) sort: MatSort ;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   allComplete: boolean = false;
 
-  updateAllComplete() {
-    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
-    console.log("HAHAHHAHAHAH");
-  }
-
-  constructor(
-    private getTrucksService: GetTrucksService
-  ) { }
+  constructor(private getTrucksService: GetTrucksService) {}
 
   ngAfterViewInit() {
     // @ts-ignore
@@ -69,5 +68,78 @@ export class ListTruckComponent {
     window.history.back();
   }
 
+  chooseFilter() {
+    //CLEAR THE FORM
+    this.truckCaractRef = null;
+    this.truckPlateRef = null;
 
+    if (this.filterOption == 'All Trucks') {
+      this.getTrucksByFilter();
+    }
+  }
+
+  getTrucksByFilter() {
+    //RESET THE VALUES
+    this.errorMessage = '';
+    this.error = false;
+
+    if (this.filterOption == 'Truck plate') {
+      this.getTrucksService.getTrucksByPlate(this.truckPlateRef).then(
+        (data: any) => {
+          this.trucks = data;
+        },
+        //transforms into a http error
+        (error: any) => {
+          this.error = true;
+          if (error.status == 400) {
+            this.errorMessage = error.error;
+          } else {
+            if (error.status == 500) {
+              this.errorMessage = error.error.errors.message;
+            } else {
+              this.errorMessage = 'Unknown error!';
+            }
+          }
+        }
+      );
+    } else if (this.filterOption == 'Truck characteristic') {
+      this.getTrucksService.getTrucksByCaract(this.truckCaractRef).then(
+        (data: any) => {
+          this.trucks = data;
+        },
+        //transforms into a http error
+        (error: any) => {
+          this.error = true;
+          if (error.status == 400) {
+            this.errorMessage = error.error;
+          } else {
+            if (error.status == 500) {
+              this.errorMessage = error.error.errors.message;
+            } else {
+              this.errorMessage = 'Unknown error!';
+            }
+          }
+        }
+      );
+    } else if (this.filterOption == 'All Trucks') {
+      this.getTrucksService.getTrucks().then(
+        (data: any) => {
+          this.trucks = data;
+        },
+        //transforms into a http error
+        (error: any) => {
+          this.error = true;
+          if (error.status == 400) {
+            this.errorMessage = error.error;
+          } else {
+            if (error.status == 500) {
+              this.errorMessage = error.error.errors.message;
+            } else {
+              this.errorMessage = 'Unknown error!';
+            }
+          }
+        }
+      );
+    }
+  }
 }
